d2b9abead0719083c8a26fac704e37cccd6b1cea
Gabriele31201 <gabri31201@gmail.com>
2023-04-04 15:31:17 +0200

    add prompt for list product

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let arrayList = [];
+
+let addShoppingList = prompt ("Inserisci i prodotti della tua lista:");
+
+for(let i = 0; i > addShoppingList; i++) {
+   
+ 
+
+
+}
+
+
+console.log(addShoppingList)
